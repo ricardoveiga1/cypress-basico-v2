8633d079e0bbe5fa89ff8db7167c8b177757cc34
ricardoveiga1 <ricardoveiga.ti@gmail.com>
2022-03-16 00:09:57 -0300

    Utilizando Lodash para executar mesmo teste várias vezes

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,11 @@
-it.only('testa a página da política de privavidade de forma independente', function(){
-    cy.visit( './src/privacy.html')
-    cy.contains('Talking About Testing').should('be.visible')
+
+//o teste será executado 5 vezes, lib Lodash
+Cypress._.times(5, function() {
+
+    it('testa a página da política de privavidade de forma independente', function(){
+        cy.visit( './src/privacy.html')
+        cy.contains('Talking About Testing').should('be.visible')
+    
+    })
 
 })
